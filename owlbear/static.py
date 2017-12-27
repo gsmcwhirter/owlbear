c879c9baebfb4a6e8dd461cedf3e407524a40a74
Gregory McWhirter <greg@ideafreemonoid.org>
2017-12-27 02:17:43 -0500

    Add custom mimetype mappings and make sure a None value can't be set for the content_type

--- owlbear/static.py
+++ owlbear/static.py
@@ -13,6 +13,23 @@ from owlbear.types import RequestHandler
 
 
 mimetypes.init()
+# https://stackoverflow.com/questions/2871655/proper-mime-type-for-fonts/10864297#10864297
+# svg   as "image/svg+xml"                  (W3C: August 2011)
+# ttf   as "application/x-font-ttf"         (IANA: March 2013)
+#       or "application/x-font-truetype"
+# otf   as "application/x-font-opentype"    (IANA: March 2013)
+# woff  as "application/font-woff"          (IANA: January 2013)
+# woff2 as "application/font-woff2"         (W3C W./E.Draft: May 2014/March 2016)
+# eot   as "application/vnd.ms-fontobject"  (IANA: December 2005)
+# sfnt  as "application/font-sfnt"          (IANA: March 2013)
+
+mimetypes.add_type("image/svg+xml", "svg", strict=False)
+mimetypes.add_type("application/x-font-ttf", "ttf", strict=False)
+mimetypes.add_type("application/x-font-opentype", "otf", strict=False)
+mimetypes.add_type("application/font-woff", "woff", strict=False)
+mimetypes.add_type("application/font-woff2", "woff2", strict=False)
+mimetypes.add_type("application/vnd.ms-fontobject", "eot", strict=False)
+mimetypes.add_type("application/font-sfnt", "sfnt", strict=False)
 
 
 class StaticFileHandler:
@@ -55,7 +72,7 @@ class StaticFileHandler:
 
         guess = mimetypes.guess_type(local_path, strict=False)
         if guess is not None:
-            resp.content_type = guess[0]
+            resp.content_type = guess[0] or "application/octet-stream"
         else:
             resp.content_type = "application/octet-stream"
 
